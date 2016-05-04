a46f17e54254cca2f42c3632da9c64b6ea60f37d
Stanislav Sorokin <ssss4-281094@yandex.ru>
2016-05-04 12:20:21 +0300

    ebooks and recursion stability version

--- resources/views/library/ebooks/book.blade.php
+++ resources/views/library/ebooks/book.blade.php
@@ -1,4 +1,7 @@
 <td style="width:200px">
+    <p>{!! HTML::image('img/library/'.$row["coverImg"], 'ebook', array('style' => 'border-color: transparent; float:left; height:200px; width:150px')) !!}</p>
+
+
 
     <h4>{!! HTML::link($row['link'], $row['title']) !!}
     </h4>
--- resources/views/library/ebooks/ebook.blade.php
+++ resources/views/library/ebooks/ebook.blade.php
@@ -2,43 +2,43 @@
 
 <table class="table table-bordered no-margin" border-color="transparent" ; cellpadding="10" border-spacing=" 7px 11px" align="center" border="0px" width="100%"  >
 
-<tbody >
+    <tbody >
 
-<?php
-$i = 0;
-$rows = count($result);
-while ($i < $rows)
-{
-    echo '<tr style="padding:20px; margin-top:20px padding: 3px">';
-    $row = $result[$i++];
-    echo view('library.ebooks.book', compact('row'));
-
-    //view
-    //  echo '<td style="width:100px">';
-    // echo '<p>&nbsp;</p>';
-    // echo '</td>';
-
-    if ($i < $rows){
+    <?php
+    $i = 0;
+    $rows = count($result);
+    while ($i < $rows)
+    {
+        echo '<tr style="padding:20px; margin-top:20px padding: 3px">';
         $row = $result[$i++];
         echo view('library.ebooks.book', compact('row'));
-    }else{
-        echo '<td style="width:200px; padding: 3px">';
+
+        //view
+      //  echo '<td style="width:100px">';
+       // echo '<p>&nbsp;</p>';
+       // echo '</td>';
+
+        if ($i < $rows){
+            $row = $result[$i++];
+            echo view('library.ebooks.book', compact('row'));
+        }else{
+            echo '<td style="width:200px; padding: 3px">';
+        }
+
+      //  echo '<td style="width:100px">';
+      //  echo '<p>&nbsp;</p>';
+     //   echo '</td>';
+
+        if ($i < $rows){
+            $row = $result[$i++];
+            echo view('library.ebooks.book', compact('row'));
+        }else{
+            echo '<td style="width:200px; padding: 3px">';
+        }
+
+        echo '</tr>';
     }
-
-    //  echo '<td style="width:100px">';
-    //  echo '<p>&nbsp;</p>';
-    //   echo '</td>';
-
-    if ($i < $rows){
-        $row = $result[$i++];
-        echo view('library.ebooks.book', compact('row'));
-    }else{
-        echo '<td style="width:200px; padding: 3px">';
-    }
-
-    echo '</tr>';
-}
-?>
-</tbody>
-<br>
+    ?>
+    </tbody>
+    <br>
 </table>
