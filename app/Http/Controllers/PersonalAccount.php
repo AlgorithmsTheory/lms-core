b3fafc2872b929c3322436f9eaf6e81f28abf758
Personioto <80115415+Personioto@users.noreply.github.com>
2021-11-05 16:31:33 +0300

    teacher_account filter problem solved

--- app/Http/Controllers/PersonalAccount.php
+++ app/Http/Controllers/PersonalAccount.php
@@ -4,6 +4,7 @@ use App\Testing\Result;
 use App\Testing\Test;
 use App\User;
 use Auth;
+use Illuminate\Http\Request;
 use Session;
 class PersonalAccount extends Controller{
     private $test;
@@ -29,7 +30,7 @@ class PersonalAccount extends Controller{
 
 
 
-    public function showAllTests(){   //показывает результыты всех тестов всех студентов
+    public function showAllTests(Request $request){   //показывает результаты всех тестов всех студентов
         $tests = [];
         $names = [];
         $last_names = [];
@@ -47,27 +48,53 @@ class PersonalAccount extends Controller{
             }
         }
         $amount = count($tests);
-        $c = 0;
-        $resultsQuery = Result::join('tests', 'tests.id_test', '=', 'results.id_test')->select('id', 'test_name', 'result_date', 'result', 'mark_eu')->get();
+        $resultsQuery = Result::join('tests', 'tests.id_test', '=', 'results.id_test');
+        if (!empty($request->test)) {
+            $resultsQuery = $resultsQuery->where('tests.id_test', $request->test);
+        }
+        $resultsQuery = $resultsQuery
+            ->leftJoin('users', 'users.id', '=', 'results.id')
+            ->leftJoin('groups', 'groups.group_id', '=', 'users.group');
+
+        if (!empty($request->surname)) {
+            $resultsQuery = $resultsQuery->where('users.last_name', 'like', '%' . $request->surname . '%');
+        }
+
+        if (!empty($request->group)) {
+            $resultsQuery = $resultsQuery->where('groups.group_name', 'like', '%' . $request->group . '%');
+        }
+
+        $request_test = $request->test;
+        $request_surname = $request->surname;
+        $request_group = $request->group;
+
+        $resultsQuery = $resultsQuery
+            ->select('results.id', 'test_name', 'result_date', 'result', 'mark_eu',
+                'users.last_name', 'users.first_name', 'groups.group_name')
+            ->paginate(100);
         foreach ($resultsQuery as $res){
-            $user = User::whereId($res->id)->join('groups', 'groups.group_id', '=', 'users.group')->get();
-//            $c = $c + count($user);
-            if(count($user) != 0) {
-                array_push($last_names, $user[0]->last_name);
-                array_push($first_names, $user[0]->first_name);
-                array_push($groups, $user[0]->group_name);
-            }
-            else {
-                array_push($last_names, 'УДАЛЕН');
-                array_push($first_names, 'УДАЛЕН');
-                array_push($groups, '-1');
-            }
+            #$user = User::whereId($res->id)->join('groups', 'groups.group_id', '=', 'users.group')->get();
+            #if(isset($res->last_name)) {
+            #    array_push($last_names, $user[0]->last_name);
+            #    array_push($first_names, $user[0]->first_name);
+            #    array_push($groups, $user[0]->group_name);
+            #}
+            #else {
+            #    array_push($last_names, 'УДАЛЕН');
+            #    array_push($first_names, 'УДАЛЕН');
+            #    array_push($groups, '-1');
+            #}
+            array_push($last_names, $res->last_name);
+            array_push($first_names, $res->first_name);
+            array_push($groups, $res->group_name);
             array_push($test_names, $res->test_name);
             array_push($result_dates, $res->result_date);
             array_push($results, $res->result);
             array_push($marks, $res->mark_eu);
         }
-        return view('personal_account/teacher_account', compact('results', 'last_names', 'first_names', 'groups', 'test_names', 'result_dates', 'marks', 'amount', 'tests', 'names'));
+        return view('personal_account/teacher_account', compact('results', 'last_names',
+            'first_names', 'groups', 'test_names', 'result_dates', 'marks', 'amount', 'tests', 'names',
+            'resultsQuery', 'request_test', 'request_surname', 'request_group'));
     }
 
 }
